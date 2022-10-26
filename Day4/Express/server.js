25eaebe523ee1355f0e06be030725e257cc6ce2c
kisay97 <kisay97@naver.com>
2022-10-26 12:26:09 +0900

    코드 정리

--- Day4/Express/server.js
+++ Day4/Express/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const fs = require('fs');
 
 // 서버 시작함수.
-const start = (req,res) => {
+const start = () => {
     
     // 객체 생성.
     const app = express();
@@ -12,19 +12,14 @@ const start = (req,res) => {
     app.use((req,res) => {
 
         //  GET 요청을 통해 전달된 파라미터 파싱(읽기).
-        // console.log(req.param('name')); //Deprecated
         console.log(req.query.age);
 
-        // res.send('Hello Express Server');
         fs.readFile('./index.html',(err,data)=>{
             if(err){
                 res.status(500).send('<h1>500 Internal Server Error</h1>'+
                 '<p>'+(err.toString())+'</p>')
             }
             else {
-                // res.type('.html');
-                // res.status(200);
-                // res.send(data);
                 res.send(data.toString());
             }
         });
